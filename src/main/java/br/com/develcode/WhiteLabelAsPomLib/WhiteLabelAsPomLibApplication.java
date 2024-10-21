c3840509e9dc30ae75a622b615bf1559dcfc6f5c
jhonatan-colina-develcode <104768124+jhonatan-colina-develcode@users.noreply.github.com>
2024-10-21 16:28:29 -0300

    Firt Commit

---
+++ src/main/java/br/com/develcode/WhiteLabelAsPomLib/WhiteLabelAsPomLibApplication.java
@@ -0,0 +1,13 @@
+package br.com.develcode.WhiteLabelAsPomLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteLabelAsPomLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteLabelAsPomLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/develcode/WhiteLabelAsPomLib/WhiteLabelAsPomLibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.develcode.WhiteLabelAsPomLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteLabelAsPomLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
