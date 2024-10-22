1fe9f82694e0c0849cf3826fd6173b22eaa4a568
Jhonatan Colina <jhonatan.colina@develcode.com.br>
2024-10-22 11:26:03 -0300

    commit

--- src/test/java/br/com/develcode/whitelabel/WhiteLabelAsPomLibApplicationTests.java
+++ src/test/java/br/com/develcode/whitelabel/WhiteLabelAsPomLibApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.develcode.WhiteLabelAsPomLib;
+package br.com.develcode.whitelabel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
