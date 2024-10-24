8e3d3597d5f1177bff8eb783fbd8b60f926fc15b
Jhonatan Colina <jhonatan.colina@develcode.com.br>
2024-10-24 14:58:13 -0300

    commit

--- src/main/java/br/com/develcode/whitelabel/controller/WhiteLabelController.java
+++ src/main/java/br/com/develcode/whitelabel/controller/WhiteLabelController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test3")
+@RequestMapping("/test4")
 public class WhiteLabelController {
 
     @GetMapping
     public String index() {
-        return "Hello World v1!";
+        return "Hello World v2!";
     }
 }
